2340da02f6ee3f225886f3d58ded0b0805a48094
Rishikesh M <rishikesh.m2021@vitbhopal.ac.in>
2024-04-11 22:43:12 +0530

    Update Main.cpp

--- LearningVulkan/Main.cpp
+++ LearningVulkan/Main.cpp
@@ -10,53 +10,60 @@ int main()
 {
 	VkInstance vkInstance;
 
-	uint32_t extensionCount = 0;
-	uint32_t layerCount = 0;
-
-	std::vector<const char*> extensions;
-	std::vector<const char*> layers;
-
-	if (vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr) != VK_SUCCESS)
-	{
-		throw std::runtime_error("Failed to enumerate extensions")
-	}
-
-	std::vector<VkExtensionProperties> extensionProperties(extensionCount);
-
-	if (vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensionProperties) != VK_SUCCESS)
-	{
-		throw std::runtime_error("Failed to get extensions properties")
-	}
-
-	for (auto& extension : extensionProperties)
-	{
-		extensions.insert(extension.extensionName);
-	}
-
 	VkApplicationInfo vkAppInfo = {};
 	vkAppInfo.sType					= VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	vkAppInfo.pNext					= nullptr;
 	vkAppInfo.pApplicationName		= "LearningVulkan";
-	vkAppInfo.applicationVersion	= VK_MAKE_VERSION(1, 3, 0);
+	vkAppInfo.applicationVersion	= VK_MAKE_VERSION(1, 0, 0);
 	vkAppInfo.pEngineName			= "No Engine";
-	vkAppInfo.engineVersion			= 0;
+	vkAppInfo.engineVersion			= VK_MAKE_VERSION(1, 0, 0);
 	vkAppInfo.apiVersion			= VK_MAKE_API_VERSION(0, 1, 3, 0);
 
+	//Get extension count
+	//uint32_t extensionCount = 0;
+	//const char** extensions;
+
+	//Validation layers
+
+	//Debug Messenger
+	#ifdef _DEBUG
+	VkDebugUtilsMessengerEXT vkDebugMessenger;
+
+	VkDebugUtilsMessengerCreateInfoEXT vkDebugMessengerInfo = {};
+	vkDebugMessengerInfo.sType				= VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	vkDebugMessengerInfo.pNext				= nullptr;
+	//vkDebugMessengerInfo.flags				= ;
+	vkDebugMessengerInfo.messageSeverity	= VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	vkDebugMessengerInfo.messageType		= VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	vkDebugMessengerInfo.pfnUserCallback	= Renderer::DebugCallback;
+	vkDebugMessengerInfo.pUserData			= nullptr;
+	
+	if (vkCreateDebugUtilsMessengerEXT(vkInstance, &vkDebugMessengerInfo, nullptr, &vkDebugMessenger) != VK_SUCCESS)
+	{
+		throw std::runtime_error("Failed to create debug messenger");
+	}
+	#endif // _DEBUG
+
+
 	VkInstanceCreateInfo vkInstanceInfo = {};
-	vkInstanceInfo.sType					 = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	//vkInstanceInfo.pNext					 = nullptr;
-	//vkInstanceInfo.flags					 = 0;
-	vkInstanceInfo.pApplicationInfo			 = &vkAppInfo;
-	//vkInstanceInfo.enabledLayerCount		 = layerCount;
-	//vkInstanceInfo.ppEnabledLayerNames	 = layers;
-	//vkInstanceInfo.enabledExtensionCount	 = extensionCount;
-	//vkInstanceInfo.ppEnabledExtensionNames = extensions;
+	vkInstanceInfo.sType					= VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	//vkInstanceInfo.pNext					= nullptr;
+	//vkInstanceInfo.flags					= 0;
+	vkInstanceInfo.pApplicationInfo			= &vkAppInfo;
+	//vkInstanceInfo.enabledLayerCount		= layerCount;
+	//vkInstanceInfo.ppEnabledLayerNames	= layers;
+	//vkInstanceInfo.enabledExtensionCount	= extensionCount;
+	//vkInstanceInfo.ppEnabledExtensionNames= extensions;
 
 	if (vkCreateInstance(&vkInstanceInfo, nullptr, &vkInstance) != VK_SUCCESS)
 	{
 		throw std::runtime_error("Failed to create vulkan instance");
 	}
 	
+	#ifdef _DEBUG
+	vkDestroyDebugUtilsMessengerEXT(vkInstance, vkDebugMessenger, nullptr);
+	#endif // _DEBUG
+
 	vkDestroyInstance(vkInstance, nullptr);
 
 	return 0;
@@ -66,41 +73,15 @@ class Renderer
 {
 
 public:
-	void Initialize()
-	{
-		//App info
-		VkApplicationInfo vkAppInfo = {};
-		vkAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		vkAppInfo.pNext = nullptr;
-		vkAppInfo.pApplicationName = "LearningVulkan";
-		vkAppInfo.applicationVersion = VK_MAKE_VERSION(1, 3, 0);
-		vkAppInfo.pEngineName = "No Engine";
-		vkAppInfo.engineVersion = 0;
-		vkAppInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 3, 0);
-
-		//Create Instance
-
-		VkInstanceCreateInfo vkInstanceInfo = {};
-		vkInstanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		//vkInstanceInfo.pNext					 = nullptr;
-		//vkInstanceInfo.flags					 = 0;
-		vkInstanceInfo.pApplicationInfo = &vkAppInfo;
-		//vkInstanceInfo.enabledLayerCount		 = layerCount;
-		//vkInstanceInfo.ppEnabledLayerNames	 = layers;
-		//vkInstanceInfo.enabledExtensionCount	 = extensionCount;
-		//vkInstanceInfo.ppEnabledExtensionNames = extensions;
-
-		if (vkCreateInstance(&vkInstanceInfo, nullptr, &instance) != VK_SUCCESS)
-		{
-			throw std::runtime_error("Failed to create vulkan instance");
-		}
-	}
+	void Initialize();
 	void Shutdown();
 
 private:
 	inline void GetExtensions();
 	inline void GetLayers();
 	inline void CheckValidationLayerSupport();
+
+	static void DebugCallback();
 
 private:
 	VkInstance instance;
@@ -112,5 +93,6 @@ private:
 
 	#if _DEBUG
 	std::vector<const char*> validationLayers;
+	VkDebugUtilsMessengerEXT debugMessenger;
 	#endif // DEBUG
 };
